5b554cf181af0e7d74eea5e33baf85a37eb0f4f0
SKJoy <SKJoy2001@GMail.Com>
2026-04-21 16:03:53 +0000

    Fix PHP-FPM config: copy from template to conf.d, don't mount directory
    - Change approach: don't mount Config/PHP/ directly to /etc/php/8.4/fpm/conf.d
    - Instead, copy /Data/PHP.ini (or template if not present) to conf.d/99-custom.ini
    - This preserves existing .ini files while allowing user customization
    - Update docker-compose.yml to remove directory mapping
    - Update .gitignore to exclude PHP.ini template
    - Add PHP config section to Project.md documentation

--- Build/Volume/System/Start.sh
+++ Build/Volume/System/Start.sh
@@ -3,6 +3,13 @@
 # Copy default files from /System/Default to / without overwriting existing files
 cp -n /System/Default/* /
 
+# Copy PHP config to PHP-FPM conf.d directory (from Data if exists, else template)
+if [ -f /Data/PHP.ini ]; then
+    cp -f /Data/PHP.ini /etc/php/8.4/fpm/conf.d/99-custom.ini
+else
+    cp -f /System/Default/Data/PHP.ini /etc/php/8.4/fpm/conf.d/99-custom.ini
+fi
+
 # Manual changes by me to make code more organized and manageable
 bash OLLaMA-Start.sh
 bash OLLaMA-Model-Manage.sh
